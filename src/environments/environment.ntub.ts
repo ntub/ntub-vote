efcba53fcbc55a8a48bcfaf363b19056f038308d
Lux Protoss <zerox12311@gmail.com>
2019-05-13 09:51:32 +0800

    Modify: add build configration ntub to angular.json

---
+++ src/environments/environment.ntub.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA9H3FjbhSExysAnO4nYOT3fDRyIylBsCA',
+    authDomain: 'ntub-vote.firebaseapp.com',
+    databaseURL: 'https://ntub-vote.firebaseio.com',
+    projectId: 'ntub-vote',
+    storageBucket: 'ntub-vote.appspot.com',
+    messagingSenderId: '180728485009'
+  },
+  apiServer: 'https://ntub-vote-server.ntub.edu.tw'
+};
